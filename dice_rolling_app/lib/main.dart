e33dc33ddcd01ac3fc1362516c947ac0a36ea9b0
shane <shaneshaji10@gmail.com>
2023-08-10 12:04:33 +0530

    custom widgets/class

--- dice_rolling_app/lib/main.dart
+++ dice_rolling_app/lib/main.dart
@@ -4,10 +4,32 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello world!'),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [Color.fromARGB(255, 9, 37, 59), Colors.blueAccent],
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+              color: Colors.white, fontSize: 28, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
 }
